c0557548b0e09cff24af83027aec98bc3c79a9c1
Sam <samuel_morgan_@outlook.com>
2020-10-03 16:50:12 +0100

    Correct html structure for table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,24 +7,28 @@ const Table = (props) => {
   const columns = ["book", "author", "language", "year", "publisher"];
   return (
     <table className="table">
-      <tr>
-        {columns.map((el) => (
-          <th className="table-cell">{capitalise(el)}</th>
-        ))}
-      </tr>
-      {books.map((el) => (
+      <thead>
         <tr>
-          <td className="table-cell">{el.title}</td>
-          <td className="table-cell">{el.author_name}</td>
-          <td className="table-cell">
-            {el.language ? el.language.toString() : null}
-          </td>
-          <td className="table-cell">{el.first_publish_year}</td>
-          <td className="table-cell">
-            {el.publisher ? el.publisher.toString() : null}
-          </td>
+          {columns.map((el) => (
+            <th className="table-cell">{capitalise(el)}</th>
+          ))}
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {books.map((el) => (
+          <tr>
+            <td className="table-cell">{el.title}</td>
+            <td className="table-cell">{el.author_name}</td>
+            <td className="table-cell">
+              {el.language ? el.language.toString() : null}
+            </td>
+            <td className="table-cell">{el.first_publish_year}</td>
+            <td className="table-cell">
+              {el.publisher ? el.publisher.toString() : null}
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
