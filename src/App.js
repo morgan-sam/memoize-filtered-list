3057a1f0ee400bae7fc22a09ea74c26fa378beb2
Sam <samuel_morgan_@outlook.com>
2020-10-03 16:24:29 +0100

    Prints out list of books

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">Hey</div>;
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const getBooks = async () => {
+      const res = await fetch(" http://openlibrary.org/search.json?q=fantasy");
+      const json = await res.json();
+      console.log(json.docs);
+      setBooks(json.docs);
+    };
+    getBooks();
+  }, []);
+
+  return (
+    <div className="App">
+      {books.map((el) => (
+        <div>{el.title}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
