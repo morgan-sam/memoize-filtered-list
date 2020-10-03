1d30da82bdaa63745bd1ae0cc241ff4bac2646de
Sam <samuel_morgan_@outlook.com>
2020-10-03 16:39:07 +0100

    Basic table styling

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,22 +1,27 @@
 import React from "react";
+import "css/table.css";
 
 const Table = (props) => {
   const { books } = props;
   const columns = ["book", "author", "language", "year", "publisher"];
   return (
-    <table>
+    <table className="table">
       <tr>
         {columns.map((el) => (
-          <th>{el}</th>
+          <th className="table-cell">{el}</th>
         ))}
       </tr>
       {books.map((el) => (
         <tr>
-          <td>{el.title}</td>
-          <td>{el.author_name}</td>
-          <td>{el.language ? el.language.toString() : null}</td>
-          <td>{el.first_publish_year}</td>
-          <td>{el.publisher ? el.publisher.toString() : null}</td>
+          <td className="table-cell">{el.title}</td>
+          <td className="table-cell">{el.author_name}</td>
+          <td className="table-cell">
+            {el.language ? el.language.toString() : null}
+          </td>
+          <td className="table-cell">{el.first_publish_year}</td>
+          <td className="table-cell">
+            {el.publisher ? el.publisher.toString() : null}
+          </td>
         </tr>
       ))}
     </table>
