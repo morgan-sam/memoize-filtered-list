1d54300aa07162e49f295327b92e2dca0eb8a3e1
Sam <samuel_morgan_@outlook.com>
2020-10-06 22:35:34 +0100

    Filter disabled on search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="app">
       <Search {...{ fetchBooks, loading }} />
-      <Filter />
+      <Filter {...{ loading }} />
       {loading ? <Loading /> : <Table {...{ books }} />}
     </div>
   );
