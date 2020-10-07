4884787f6c07a7a95946083229e3104e2076b108
Sam <samuel_morgan_@outlook.com>
2020-10-07 21:05:43 +0100

    shorten fn working

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,6 +5,13 @@ import { capitalise } from "js/utility";
 const Table = (props) => {
   const { books } = props;
   const columns = ["book", "author", "language", "year", "publisher"];
+
+  const shorten = (str) => {
+    if (!str) return null;
+    else if (str.length > 30) return str.slice(0, 30) + "...";
+    else return str;
+  };
+
   return (
     <table className="table">
       <thead>
@@ -19,14 +26,16 @@ const Table = (props) => {
       <tbody>
         {books.map((el, i) => (
           <tr key={i}>
-            <td className="table-cell">{el.title}</td>
-            <td className="table-cell">{el.author_name}</td>
+            <td className="table-cell">{shorten(el.title)}</td>
             <td className="table-cell">
-              {el.language ? el.language.toString() : null}
+              {el.author_name ? shorten(el.author_name.join(" ")) : null}
             </td>
-            <td className="table-cell">{el.first_publish_year}</td>
             <td className="table-cell">
-              {el.publisher ? el.publisher.toString() : null}
+              {el.language ? shorten(el.language.toString()) : null}
+            </td>
+            <td className="table-cell">{shorten(el.first_publish_year)}</td>
+            <td className="table-cell">
+              {el.publisher ? shorten(el.publisher.toString()) : null}
             </td>
           </tr>
         ))}
