d83c6f96ea3003050bd9f1954e893365625f920e
Sam <samuel_morgan_@outlook.com>
2020-10-09 19:49:16 +0100

    Cell comp

---
+++ src/components/Cell.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const MAX_CELL_ENTRY_LENGTH = 30;
+
+const Cell = (props) => {
+  const { value, cellKey } = props;
+
+  const shorten = (str) => {
+    if (str === undefined) return null;
+    else if (str.length > MAX_CELL_ENTRY_LENGTH)
+      return str.slice(0, MAX_CELL_ENTRY_LENGTH) + "...";
+    else return str;
+  };
+
+  const formatContents = (value) => {
+    if (typeof value !== "number" && value === undefined) return null;
+    else if (Array.isArray(value) && value.length > 0)
+      return shorten(value.join(", "));
+    else return shorten(value);
+  };
+
+  return (
+    <td key={cellKey} className="table-cell">
+      {formatContents(value)}
+    </td>
+  );
+};
+
+export default Cell;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,40 +1,11 @@
 import React from "react";
 import "css/table.css";
 import { capitalise } from "js/utility";
-
-const MAX_CELL_ENTRY_LENGTH = 30;
+import Cell from "components/Cell";
 
 const Table = (props) => {
   const { books } = props;
   const columns = ["ID", "book", "author", "language", "year", "publisher"];
-
-  const shorten = (str) => {
-    if (!str) return null;
-    else if (str.length > MAX_CELL_ENTRY_LENGTH)
-      return str.slice(0, MAX_CELL_ENTRY_LENGTH) + "...";
-    else return str;
-  };
-
-  const generateCell = (value, key) => {
-    if (value == null)
-      return (
-        <td key={key} className="table-cell">
-          {" "}
-        </td>
-      );
-    else if (Array.isArray(value) && value.length > 0)
-      return (
-        <td key={key} className="table-cell">
-          {shorten(value.join(", "))}
-        </td>
-      );
-    else
-      return (
-        <td key={key} className="table-cell">
-          {shorten(value)}
-        </td>
-      );
-  };
 
   return (
     <table className="table">
@@ -57,7 +28,9 @@ const Table = (props) => {
               el.language,
               el.first_publish_year,
               el.publisher,
-            ].map((val, key) => generateCell(val, key))}
+            ].map((value, cellKey) => (
+              <Cell {...{ value, cellKey }} />
+            ))}
           </tr>
         ))}
       </tbody>
