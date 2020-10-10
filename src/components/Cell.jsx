d62c7fe839133c4cbe53f747bbd6e6b22b868469
Sam <samuel_morgan_@outlook.com>
2020-10-10 17:06:49 +0100

    Shorten fn

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -5,12 +5,14 @@ const MAX_CELL_ENTRY_LENGTH = 30;
 const Cell = (props) => {
   const { value } = props;
 
+  const shorten = (str) => str.slice(0, MAX_CELL_ENTRY_LENGTH) + "...";
+
   const getCell = (str) => {
     if (str === undefined) return <td className="table-cell" />;
     else if (str.length > MAX_CELL_ENTRY_LENGTH)
       return (
         <td className="table-cell" title={str}>
-          {str.slice(0, MAX_CELL_ENTRY_LENGTH) + "..."}
+          {shorten(str)}
         </td>
       );
     else return <td className="table-cell">{str}</td>;
