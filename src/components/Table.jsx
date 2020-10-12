ff27ddf540bf4fc54930379e22a5dee5de3bd65a
Sam <samuel_morgan_@outlook.com>
2020-10-12 22:46:33 +0100

    Table does not show header if no books

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,24 +7,26 @@ const Table = (props) => {
   const { books } = props;
   const columns = ["ID", "book", "author", "language", "year", "publisher"];
 
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          {columns.map((el, i) => (
-            <th key={i} className="table-cell heading">
-              {capitalise(el)}
-            </th>
+  if (books.length > 0)
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            {columns.map((el, i) => (
+              <th key={i} className="table-cell heading">
+                {capitalise(el)}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((entry, entryKey) => (
+            <Entry {...{ entry, entryKey }} key={entryKey} />
           ))}
-        </tr>
-      </thead>
-      <tbody>
-        {books.map((entry, entryKey) => (
-          <Entry {...{ entry, entryKey }} key={entryKey} />
-        ))}
-      </tbody>
-    </table>
-  );
+        </tbody>
+      </table>
+    );
+  else return null;
 };
 
 export default Table;
